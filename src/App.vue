0a4be18bb02ef28a16545540bf5f3b0e9fb25bbc
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2019-09-07 15:33:18 +0200

    Create game component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
+    <game/>
   </div>
 </template>
 
 <script>
 
+import Game from "@/components/Game"
+
 export default {
   name: 'app',
   components: {
+    Game
   }
 };
 </script>
---
+++ src/components/Game.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="game">
+    Bonjour
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'game',
+};
+
+</script>
+
+<style>
+
+.game {
+  width: 100%;
+}
+
+</style>
